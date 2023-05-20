ec86c74fca0e2b5c8720070d4e19ef178e500f84
Chance Amos <66534471+astroChance@users.noreply.github.com>
2023-05-20 14:00:42 -0500

    initial packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import re
+
+package_name = "rputils"
+
+VERSIONFILE=package_name+"/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in ", VERSIONFILE)
+
+setup(name=package_name,
+      version= verstr,
+      description='Library of rock physics utilities',
+      author='Chance Amos',
+      author_email='camos@mines.edu',
+      packages = find_packages())
